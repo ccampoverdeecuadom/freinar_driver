dfe89b350da068c9766526cab69c3a026492f7db
Victor <w.cev@ecuadom.com>
2021-01-28 14:53:17 -0500

    first

---
+++ android/app/src/main/kotlin/com/gomarketdemo/driver/ApplicationStatus.java
@@ -0,0 +1,28 @@
+package com.gomarketdemo.driver;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import io.flutter.app.FlutterApplication;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+
+public class ApplicationStatus extends FlutterApplication implements PluginRegistry.PluginRegistrantCallback {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setApplicationId("com.gomarketdemo.driver") // Required for Analytics.
+                .setProjectId("gomarketdemo") // Required for Firebase Installations.
+                .setApiKey("put your key here") // Required for Auth.
+                .build();
+        FirebaseApp.initializeApp(this,options,"gomarketdemo");
+        FlutterFirebaseMessagingService.setPluginRegistrant(this);
+    }
+
+    @Override
+    public void registerWith(PluginRegistry registry) {
+        FirebaseCloudMessagingPluginRegistrant.registerWith(registry);
+    }
+}
